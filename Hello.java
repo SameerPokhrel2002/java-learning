655555485f6c50f4a7be5de51d33bf43091891bc
Sameer Pokhrel <sameerpokhrel2002@gmail.com>
2025-04-30 09:02:32 +0545

    initial commit of java learning files

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+     public static void main(String[] args)
+     {
+        System.out.println("how are you?");
+        System.out.println("sameer pokhrel");
+        boolean isAlien=false;
+        if(isAlien==false)
+        {
+            System.out.println("It is not an alien!");
+        }
+     }
+}
