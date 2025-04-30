5fa36f91bef1cf1f5753f704dc545e6d319d191d
Sameer Pokhrel <sameerpokhrel2002@gmail.com>
2025-04-30 11:40:26 +0545

    simple ternary operator use

---
+++ Volkswagen.java
@@ -0,0 +1,10 @@
+public class Volkswagen {
+    public static void main(String[] args) {
+        String makeOfCar="Volkswagen";
+        boolean isDomestic=(makeOfCar=="Volkswagen")?false:true;
+        if(!isDomestic)
+        {
+            System.out.println("The make of Car is not domestic!");
+        }
+    }
+}
