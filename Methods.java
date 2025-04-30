ab588326e20c83c6b990d4fd294e2019849df4de
Sameer Pokhrel <sameerpokhrel2002@gmail.com>
2025-04-30 20:13:51 +0545

    learnt about methods

---
+++ Methods.java
@@ -0,0 +1,19 @@
+public class Methods {
+    public static void main(String[] args) {
+        calculateScore(true,800,5,100); //invoking the method 
+        calculateScore(true, 40, 7, 700);
+        
+    }
+    //in java, unlike other programming languages like c#, a method can't be defined inside another method like in main method
+    public static void calculateScore(boolean gameOver, int score,int levelCompleted, int bonus){
+      
+        int finalScore=score;
+
+        if(gameOver){
+            finalScore+=(levelCompleted*bonus);
+            finalScore+=100;
+            System.out.println("Your final score was "+ finalScore);
+        }
+        
+    }
+    }
