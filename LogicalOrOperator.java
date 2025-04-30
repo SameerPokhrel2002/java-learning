ff9526926a288d236bd7bf808d8d7124d2761398
Sameer Pokhrel <sameerpokhrel2002@gmail.com>
2025-04-30 09:51:16 +0545

    learnt about logical or operator

---
+++ LogicalOrOperator.java
@@ -0,0 +1,11 @@
+public class LogicalOrOperator {
+    public static void main(String[] args) {
+        
+        int topScore=60;
+        int secondTopScore=81;
+        if (topScore>90 || secondTopScore<=90)
+        {
+            System.out.println("Either or both of the conditions are true!");
+        }
+    }
+}
---
+++ OperatorPrecedence.java
@@ -0,0 +1,5 @@
+public class OperatorPrecedence {
+    public static void main(String[] args) {
+        
+    }
+}
