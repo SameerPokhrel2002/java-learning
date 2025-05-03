f319da390d5af921c701052f05af4c68df889de0
Sameer Pokhrel <sameerpokhrel2002@gmail.com>
2025-05-03 07:40:37 +0545

    learnt about switch case statement in java

---
+++ Switch.java
@@ -0,0 +1,49 @@
+public class Switch {
+    public static void main(String[] args) {
+        int day = 3;
+        String dayName;
+
+        switch (day) {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            case 7:
+                dayName = "Sunday";
+                break;
+            default:
+                dayName = "Invalid day";
+        }
+
+        System.out.println("The day is: " + dayName);
+
+        int switchValue=2;
+        switch (switchValue){
+            case 1:
+                System.out.println("Value is 1");
+                break;
+            case 2:
+                System.out.println("Value is 2");
+                break;
+            case 3:
+                System.out.println("Value is 3");
+                break;
+            default:
+                System.out.println("Value is not 1, 2 or 3");
+        }
+    }
+}
