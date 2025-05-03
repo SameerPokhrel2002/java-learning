a6fb781addebb402513cefc8ba089aebc6049cb9
Sameer Pokhrel <sameerpokhrel2002@gmail.com>
2025-05-03 08:43:56 +0545

    challenge on traditional switch

---
+++ TraditionalSwitchChallenge.java
@@ -0,0 +1,26 @@
+public class TraditionalSwitchChallenge {
+    public static void main(String[] args) {
+        char letter = 'Z';
+        switch (letter) {
+            case 'A':
+                System.out.println(letter + "=Able");
+                break;
+            case 'B':
+                System.out.println(letter + "=Baker");
+                break;
+            case 'C':
+                System.out.println(letter + "=Charlie");
+                break;
+            case 'D':
+                System.out.println(letter + "Dog");
+                break;
+            case 'E':
+                System.out.println(letter + "=Easy");
+                break;
+            default:
+                System.out.println(letter + "=!!Not Found!!");
+                break;
+
+        }
+    }
+}
