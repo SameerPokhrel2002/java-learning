9c12a149fa3965b5ee75b0755d199701bd2dc70b
Sameer Pokhrel <sameerpokhrel2002@gmail.com>
2025-05-04 09:41:12 +0545

    while challenge

--- WhileChallenge.java
+++ WhileChallenge.java
@@ -1,12 +1,18 @@
 public class WhileChallenge {
     public static void main(String[] args) {
         int num = 5;
+        int count = 0;
         while (num <= 20) {
             if (isEvenNumber(num)) {
+                count++;
                 System.out.println(num);
             }
             num++;
+            if (count == 5) {
+                break;
+            }
         }
+        System.out.println("Total number of even numbers: " + count);
     }
 
     public static boolean isEvenNumber(int num) {
